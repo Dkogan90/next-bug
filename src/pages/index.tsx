00dde705123a9f0a86a4f193c7739bd567104ece
DKogan90 <david_kogan@hotmail.de>
2023-03-27 13:48:39 +0200

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import React, { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }, [])
+
   return (
     <>
       <Head>
